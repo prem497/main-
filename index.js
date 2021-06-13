480dbcf1810688621d78e7ada32ada38c9c5ab18
Premchandar <77929994+prem497@users.noreply.github.com>
2021-06-13 22:04:45 +0530

    Html css js skills  i am practicing

---
+++ index.js
@@ -0,0 +1,15 @@
+const order = document.querySelector(".order");
+
+function handleOrderClick() {
+  if (!order.classList.contains("animate")) {
+    order.classList.add("animate");
+    setTimeout(function() {
+      order.classList.remove("animate");
+    }, 10000);
+  }
+}
+
+function init() {
+  order.addEventListener("click", handleOrderClick);
+}
+init();
